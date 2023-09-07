dedcf34ba191aac526c2167c8beb95026efd356c
Sayeed <sayeedssaadman272491@gmail.com>
2023-09-07 23:47:06 +0600

    anything

--- lib/main.dart
+++ lib/main.dart
@@ -2,39 +2,37 @@
 import 'package:flutter/material.dart';
 
 void main () {
-  runApp( //pura app k run korbe..
-    /*  MaterialApp( //app er config set kore dibe..
-      home: Scaffold( //home ta holo name parameter .. and scaffold holo ekta screen/layout .
-        body: Center( //body holo shoril er moto. like html // center e niye ashse text
-          child: Text("Hello world"),
-        ),
-      ),
-    ) */
-      Myapp()
-  );
+  runApp(Myapp());
 }
-// hot reload --> cntrl + s
 class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
-      debugShowCheckedModeBanner:false, //corner debug flash.
       home: Scaffold(
-        backgroundColor: Colors.purpleAccent, //set colour to scaffold backgrnd
+        backgroundColor: Colors.white, //set colour to scaffold backgrnd
         appBar: AppBar(
-          backgroundColor: Colors.amberAccent,// appbar er clr change
+          backgroundColor: Colors.amberAccent,
           centerTitle: true, // appbar er text centre er jnno command
-          title: Text("UNGABUNGA"),
+          title: Text("Hello"),
           leading: Icon(Icons.add_business,color: Colors.amber,size: 30,), // left side hole leading + size can be added
           actions: [Icon(Icons.search),], // right hole actions + clr add kra jay
         ),
         body: Center(
-          child: Text("Hello",style: TextStyle(
-            color: Colors.black,
-            fontSize: 30,
-            fontWeight: FontWeight.bold
-          ),),
-        ),
+            child: Column( //work with col
+              mainAxisAlignment: MainAxisAlignment.center, // edit with main axis
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text("Hello world"),
+            Row( //work with row
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text("Hello"),
+                Text(" world")
+              ],
+            )
+          ],
+        )),
       ),
       title: "Valley",
     );
