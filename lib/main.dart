bd6ae2045cd21df21d5a02d4004f6142e4b9e54a
Sayeed <sayeedssaadman272491@gmail.com>
2023-09-20 01:17:01 +0600

    anything

--- lib/main.dart
+++ lib/main.dart
@@ -23,13 +23,18 @@ class Myapp extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             Text("Hello world"),
-            Row( //work with row
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text("Hello"),
-                Text(" world")
-              ],
+            Center(
+              child: Row( //work with row
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Center(child: Text("Hello")),
+                  Text(" world"),
+                 // Image.asset('Image/goat.jpg'), // go to pubspec  => asset folder
+                  Image.network('https://th.bing.com/th/id/R.ad7f4dd1d6b969d7301d3a427d36c50d?rik=DoNiHMv%2f34AgrA&riu=http%3a%2f%2fwww.brandessencenigeria.com%2fwp-content%2fuploads%2f2018%2f02%2fFifa_world_cup.png&ehk=mgKEVTlM14ksELkW%2baJ7bol4W7UyNqV5urGVqMaCBZc%3d&risl=&pid=ImgRaw&r=0',height: 200,width: 200,)
+
+                ],
+              ),
             )
           ],
         )),
